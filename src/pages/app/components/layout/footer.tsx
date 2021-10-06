20738f6bd7f314f85bcd78206bd310f8d3513c26
Otávio Lima <joseotaviodelima@hotmail.com>
2021-10-06 17:55:54 -0300

    UP06102021 v7

--- src/pages/app/components/layout/footer.tsx
+++ src/pages/app/components/layout/footer.tsx
@@ -5,7 +5,51 @@ import { ArrowRightShort } from '@styled-icons/bootstrap'
 const Footer = () => {
   return (
     <footer className="footer" style={{ backgroundColor: 'var(--color-gray)' }}>
-      FOOTER
+      <div className="footer-container container-xxl mg-t-5 mg-b-5">
+        <div className="column-6 col-12">
+          <div className="content">a</div>
+        </div>
+        <div className="column-6 col-12">
+          <div className="content">
+            <nav className="hidden-1 footer-nav">
+              <ul>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <b>Legal</b>
+                </div>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <a href="#">Terms & Conditions</a>
+                </div>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <a href="#">Privacy Policy</a>
+                </div>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <a href="#">Licenses</a>
+                </div>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <a href="#">Copyright Notice & Disclaimer</a>
+                </div>
+              </ul>
+              <ul>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <b>Markets</b>
+                </div>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <a href="#">Terms & Conditions</a>
+                </div>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <a href="#">Privacy Policy</a>
+                </div>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <a href="#">Licenses</a>
+                </div>
+                <div className="footer-menu-item pd-3 pd-l-5 pd-r-5">
+                  <a href="#">Copyright Notice & Disclaimer</a>
+                </div>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
