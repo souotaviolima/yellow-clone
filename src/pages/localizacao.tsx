4df954540dc22c382bf8dbfc0a1637b9ac2f0b4b
Otávio Lima <joseotaviodelima@hotmail.com>
2021-11-26 11:49:44 -0300

    UP26112021 v1

--- src/pages/localizacao.tsx
+++ src/pages/localizacao.tsx
@@ -12,22 +12,101 @@ export default function Index() {
         title="Localização"
         small="Conheça os procedimentos oferecidos pelaaclínica"
       />
-      <div className="container-lg after">
-        <div
-          className={'content' + ' ' + StylesCustom.content_columns}
-          style={styles.columnsContent}
-        >
-          <div className="column-12 col-12">
-            <div className="content-map pd-t-6 pd-b-6">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.3845918541447!2d-35.20657448523288!3d-5.801234295796206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b3000357211167%3A0x4486822657b73d4f!2sAv.%20Romualdo%20Galv%C3%A3o%2C%20138a%20-%20Barro%20Vermelho%2C%20Natal%20-%20RN%2C%2059022-100!5e0!3m2!1spt-BR!2sbr!4v1637865757480!5m2!1spt-BR!2sbr"
-                width="600"
-                height="450"
-                style={{ border: 0 }}
-                aria-hidden="false"
-              ></iframe>
-            </div>
+      <div className="container-xxl after">
+        <div className="column-7 col-12">
+          <div className="content-map pd-t-6 pd-b-6">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.3845918541447!2d-35.20657448523288!3d-5.801234295796206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b3000357211167%3A0x4486822657b73d4f!2sAv.%20Romualdo%20Galv%C3%A3o%2C%20138a%20-%20Barro%20Vermelho%2C%20Natal%20-%20RN%2C%2059022-100!5e0!3m2!1spt-BR!2sbr!4v1637865757480!5m2!1spt-BR!2sbr"
+              width="600"
+              height="450"
+              style={{ border: 0 }}
+              aria-hidden="false"
+            ></iframe>
           </div>
+        </div>
+        <div className="column-4 col-12 ">
+          <div
+            className="header-form mg-b-5 before"
+            style={{ textAlign: 'center' }}
+          >
+            <h1
+              className="title-s-1"
+              style={{
+                fontSize: '2pc',
+                fontWeight: 400,
+                color: 'var(--theme-primary-color) !important'
+              }}
+            >
+              Agendamento Online
+            </h1>
+            <small
+              className="small-s-1"
+              style={{
+                fontSize: '1pc',
+                fontWeight: 300,
+                color: 'var(--color-grey) !important'
+              }}
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been
+            </small>
+          </div>
+          <form style={styles.form} className="after">
+            <div className="form-content" style={styles.formContent}>
+              <div className="input-group mg-b-3" style={styles.inputGroup}>
+                <div
+                  className="input-group-prepend"
+                  style={styles.groupPrepend}
+                >
+                  <span className="input-group-text" id="basic-addon1">
+                    ico
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nome*"
+                  style={styles.inputControl}
+                />
+              </div>
+              <div className="input-group mg-b-3" style={styles.inputGroup}>
+                <div
+                  className="input-group-prepend"
+                  style={styles.groupPrepend}
+                >
+                  <span className="input-group-text" id="basic-addon1">
+                    ico
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="E-mail*"
+                  style={styles.inputControl}
+                />
+              </div>
+              <div className="input-group mg-b-5" style={styles.inputGroup}>
+                <textarea
+                  className="form-control"
+                  placeholder="Mensagem*"
+                  style={styles.inputControl}
+                />
+              </div>
+              <button
+                className="btn-s-2 pd-2 pd-l-5 pd-r-5 flex-display"
+                style={{
+                  borderRadius: 50,
+                  backgroundColor: '#8f8f8f',
+                  display: 'table',
+                  margin: 'auto'
+                }}
+              >
+                <strong style={{ fontWeight: 700, color: '#ffffff' }}>
+                  AGENDAR
+                </strong>
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </Wrap>
@@ -42,9 +121,7 @@ const styles: any = {
     display: 'block',
     padding: 20,
     backgroundColor: '#ffffff',
-    paddingBottom: 100,
-    borderRadius: 15,
-    border: '1px solid #e3075e'
+    paddingBottom: 100
   },
   inputGroup: {
     display: 'flex',
